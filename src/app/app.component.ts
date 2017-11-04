4922866382e337a17d15eaea4a978a488ce3f70e
Nguyen Dinh Quang <nguoikiemtim@gmail.com>
2017-11-04 10:45:11 +0700

    Finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,12 @@ let _this;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  cityField = '';
+  districtField = '';
+  wardField = '';
+  address = '';
   mapInstance: google.maps.Map;
   title = 'app';
-  address = '';
   fstatus = false;
   geocodeRequest: google.maps.GeocoderRequest;
   isGeocodeFinished = false;
@@ -28,6 +31,7 @@ export class AppComponent implements OnInit {
     };
     const mapDiv: HTMLDivElement = <HTMLDivElement>document.getElementById('ggmap');
     this.mapInstance = new google.maps.Map(mapDiv, opt);
+    this.autoFill();
   }
 
   async waitGoogleApisLoaded(): Promise<void> {
@@ -42,8 +46,47 @@ export class AppComponent implements OnInit {
             }, milliseconds);
         });
   }
-  geocode_click() {
-    let address = (<HTMLInputElement>document.getElementById('addr')).value + ', Quan 7, Ho Chi Minh';
+
+
+  async autoFill(): Promise<void> {
+    const latlng = await this.getLocation();
+    console.log(latlng);
+
+    const service = new google.maps.Geocoder();
+    service.geocode({location: latlng}, this.autoFillCallback);
+  }
+  autoFillCallback(results, status) {
+    console.log(results[0]);
+    const formatted_address: string =  results[0].formatted_address;
+    const formatted_address_arr = formatted_address.split(', ');
+    const txt_city: HTMLInputElement = <HTMLInputElement>document.getElementById('txt_city');
+    const txt_dicstict: HTMLInputElement = <HTMLInputElement>document.getElementById('txt_dicstict');
+    const txt_ward: HTMLInputElement = <HTMLInputElement>document.getElementById('txt_ward');
+    const txt_address: HTMLInputElement = <HTMLInputElement>document.getElementById('txt_address');
+
+
+    try {
+      txt_city.value = formatted_address_arr[formatted_address_arr.length - 2];
+      _this.cityField = formatted_address_arr[formatted_address_arr.length - 2];
+      txt_dicstict.value = formatted_address_arr[formatted_address_arr.length - 3];
+      _this.districtField = formatted_address_arr[formatted_address_arr.length - 3];
+      txt_ward.value = formatted_address_arr[formatted_address_arr.length - 4];
+      _this.wardField = formatted_address_arr[formatted_address_arr.length - 4];
+      txt_address.value = formatted_address_arr[formatted_address_arr.length - 5];
+      _this.address = formatted_address_arr[formatted_address_arr.length - 5];
+    } catch (ex) {
+      console.log(ex);
+    }
+
+    console.log('1', this.address);
+  }
+  async geocode_click(): Promise<void> {
+    console.log('2', this.address);
+    let address = this.address + ', ' +
+                  this.wardField + ', ' +
+                  this.districtField + ', ' +
+                  this.cityField;
+
     address = address.replace(/ /g, '+');
 
     const textSearchReq: google.maps.places.TextSearchRequest = {
@@ -53,12 +96,10 @@ export class AppComponent implements OnInit {
     service.textSearch(textSearchReq, (results, status) => {
       this.geocodeCallback(results, address);
     });
-
   }
   geocodeCallback(results: google.maps.places.PlaceResult[], address: string) {
     if (results.length !== 0) {
       console.log(results);
-
       const markerLatLng: google.maps.LatLngLiteral = {
         lat: results[0].geometry.location.lat(),
         lng: results[0].geometry.location.lng(),
@@ -84,5 +125,42 @@ export class AppComponent implements OnInit {
         this.geocodeCallback(_results, address);
       });
     }
+  }
+  async getLocation(): Promise<google.maps.LatLngLiteral> {
+    return new Promise<google.maps.LatLngLiteral> ( resolve => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+          const markerLatLng: google.maps.LatLngLiteral = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          this.mapInstance.setCenter(markerLatLng);
+          const markersOpt: google.maps.MarkerOptions = {
+            draggable: false,
+            position: markerLatLng,
+          };
+          const user_marker = new google.maps.Marker( markersOpt);
+          user_marker.setMap(this.mapInstance);
+          this.mapInstance.setZoom(15);
+          resolve(markerLatLng);
+        });
+      }
+    });
+  }
+  onCityChange(event) {
+    this.cityField = event.target.value;
+    console.log(this.cityField);
+  }
+  onDistrictChange(event) {
+    this.districtField = event.target.value;
+    console.log(this.districtField);
+  }
+  onWardChange(event) {
+    this.wardField = event.target.value;
+    console.log(this.wardField);
+  }
+  onAddressChange(event) {
+    this.address = event.target.value;
+    console.log(this.address);
   }
 }
